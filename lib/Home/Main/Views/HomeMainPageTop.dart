db80c75d57c299e1f09d9847431cd4ab2066b98a
Alan <alan.h@hcp.tech>
2021-04-27 23:30:31 +0800

    首页顶部模块入口UI布局

--- lib/Home/Main/Views/HomeMainPageTop.dart
+++ lib/Home/Main/Views/HomeMainPageTop.dart
@@ -59,6 +59,58 @@ class HomeMainPageTop extends StatelessWidget {
         color: Colors.white,
         borderRadius: BorderRadius.all(Radius.circular(6.0))
       ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [oneLevelEntry('酒店'), oneLevelEntry('机票')],
+          ),
+          Row(
+            children: [
+              twoLevelEntry('订餐'),
+              twoLevelEntry('打车'),
+              twoLevelEntry('火车票'),
+              twoLevelEntry('加油')
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  //酒店、机票入口
+  Widget oneLevelEntry(String text) {
+    return Container(
+      color: Colors.blueGrey,
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.red,
+            // width: 100.w,
+            height: 70.h,
+          ),
+          Positioned(
+            child: Column(
+              children: [
+                Text('图片'),
+                Text(text)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  //订餐、打车、火车票、加油入口
+  Widget twoLevelEntry(String text) {
+    return Container(
+      child: Column(
+        children: [
+          Text('图片'),
+          Text(text)
+        ],
+      ),
     );
   }
 }
